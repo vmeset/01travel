a47f2dc7e5edc3614947bab27b8192263154a5e6
vmeset <vmeset@gmail.com>
2019-07-18 17:44:12 +0300

    Create Main Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './App.module.css';
 import Head from './components/header/head'
 import { BrowserRouter} from "react-router-dom";
+import Main from "./components/content/main/main";
 
 function App() {
   return (
       <BrowserRouter>
         <div className={styles.App}>
           <Head />
+          <Main />
         </div>
       </BrowserRouter>
   );
--- src/components/header/navigator/navigator.js
+++ src/components/header/navigator/navigator.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './navigator.module.css'
+import {BrowserRouter} from "react-router-dom";
 
 const Navigator = () => {
     return (
         <div className={styles.navigator}>
             <div>
-                Главная
+                <a>Главная</a>
             </div>
             <div>
                 Туры
