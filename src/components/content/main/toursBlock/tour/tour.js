6619064117bb0da5c9df90a0e91a8281e3a04c46
vmeset <vmeset@gmail.com>
2019-07-23 10:13:32 +0300

    Add toursData object to tours

--- src/components/content/main/toursBlock/tour/tour.js
+++ src/components/content/main/toursBlock/tour/tour.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import styles from './tour.module.css'
 
-const Tour = () => {
+const Tour = (props) => {
+
     return (
         <div className={styles.tour}>
             <div className={styles.tourImg}>
                 <img src='https://static.wixstatic.com/media/f315df_d61e2ca7c57a40f480ae017a5bfba90a~mv2.jpg/v1/fill/w_410,h_514,al_c,lg_1,q_80/bolshoyThachMain.webp' />
             </div>
             <div>
-                <h3>Header</h3>
+                <h3>{props.header}</h3>
             </div>
             <div>
-                <h4>Сложность</h4>
+                <h4>{props.difficulty}</h4>
             </div>
             <div>
-                <h4>Сезон</h4>
+                <h4>{props.season}</h4>
             </div>
             <div>
                 5000
--- src/components/content/main/toursBlock/toursBlock.js
+++ src/components/content/main/toursBlock/toursBlock.js
@@ -3,11 +3,20 @@ import styles from './toursBlock.module.css'
 import Tour from './tour/tour'
 
 const ToursBlock = () => {
+
+    let toursData = {
+        places: [
+            {id: 1, header: 'Fisht', difficulty: 'hard', season: 'summer'},
+            {id: 2, header: 'Oshten', difficulty: 'medium', season: 'winter'},
+            {id: 3, header: 'Tchach', difficulty: 'easy', season: 'spring'}
+        ]
+    }
+
+    let toursInfo = toursData.places.map ( ti => (<Tour id={ti.id} header={ti.header} difficulty={ti.difficulty} season={ti.season} />) )
+
     return (
         <div className={styles.toursBlock}>
-            <Tour />
-            <Tour />
-            <Tour />
+            {toursInfo}
         </div>
     )
 }
